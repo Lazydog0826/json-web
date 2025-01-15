228942458d7f5c592e711787b5b9e4bcb1906910
hzc <huangzicheng@wexsu.com>
2025-01-15 17:44:59 +0800

    feat: 优化

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="editor-container" ref="editorContainer"></div>
-  <a-modal
-    v-model:visible="model_visible"
-    @ok="handleOk"
-    :mask-closable="false"
-  >
-    <template #title>Share</template>
+
+  <a-modal v-model:visible="share_model_visible" :mask-closable="false">
+    <template #title>分享</template>
     <div>
       <a-spin :loading="loading" style="width: 100%">
         <a-space direction="vertical" style="width: 100%">
           <a-input-number
             v-model="hour"
-            placeholder="ExpirationTime(Hour)"
+            placeholder="过期时间 - 小时"
             :min="1"
             :max="10000"
           />
@@ -22,11 +19,63 @@
     </div>
     <template #footer>
       <a-space>
-        <a-button :loading="loading" @click="() => (model_visible = false)">
-          Cancel
+        <a-button
+          :loading="loading"
+          @click="() => (share_model_visible = false)"
+        >
+          取消
         </a-button>
-        <a-button :loading="loading" type="primary" @click="handleOk">
-          Submit
+        <a-button :loading="loading" type="primary" @click="shareHandle">
+          生成链接
+        </a-button>
+      </a-space>
+    </template>
+  </a-modal>
+
+  <a-modal v-model:visible="settings_visible" :mask-closable="false">
+    <template #title>设置</template>
+    <div>
+      <a-form :model="settings" layout="vertical">
+        <a-form-item field="language" label="语言">
+          <a-select v-model="settings.language" allow-search>
+            <a-option value="plaintext">PlainText</a-option>
+            <a-option value="javascript">JavaScript</a-option>
+            <a-option value="typescript">TypeScript</a-option>
+            <a-option value="css">CSS</a-option>
+            <a-option value="html">HTML</a-option>
+            <a-option value="json">JSON</a-option>
+            <a-option value="xml">XML</a-option>
+            <a-option value="yaml">YAML</a-option>
+            <a-option value="markdown">Markdown</a-option>
+            <a-option value="csharp">C#</a-option>
+            <a-option value="rust">Rust</a-option>
+            <a-option value="sql">SQL</a-option>
+            <a-option value="bash">Bash</a-option>
+            <a-option value="powershell">PowerShell</a-option>
+            <a-option value="dockerfile">Dockerfile</a-option>
+            <a-option value="less">Less</a-option>
+            <a-option value="scss">SCSS</a-option>
+            <a-option value="sass">Sass</a-option>
+            <a-option value="stylus">Stylus</a-option>
+          </a-select>
+        </a-form-item>
+        <a-form-item field="fontFamily" label="字体">
+          <a-input v-model="settings.fontFamily" />
+        </a-form-item>
+        <a-form-item field="fontSize" label="字体大小">
+          <a-input-number v-model="settings.fontSize" :min="14" :max="30" />
+        </a-form-item>
+      </a-form>
+    </div>
+    <template #footer>
+      <a-space>
+        <a-button @click="settingsCancel"> 取消 </a-button>
+        <a-button
+          :loading="loading"
+          type="primary"
+          @click="settingsHandle(true)"
+        >
+          设置
         </a-button>
       </a-space>
     </template>
@@ -34,32 +83,52 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
+import { onMounted, onUnmounted, ref, reactive } from "vue";
 import * as monaco from "monaco-editor";
 import axios from "axios";
 import { Message } from "@arco-design/web-vue";
 import useClipboard from "vue-clipboard3";
+
+import loader from "@monaco-editor/loader";
+
+loader.config({ monaco });
+loader.config({ "vs/nls": { availableLanguages: { "*": "zh-cn" } } });
 
 const { toClipboard } = useClipboard();
 
 const editorContainer = ref<HTMLDivElement | null>(null);
 let editor: monaco.editor.IStandaloneCodeEditor | null = null;
 
-const model_visible = ref(false);
+const share_model_visible = ref(false);
+const settings_visible = ref(false);
 
-const hour = ref(24);
+let currSettings = {
+  language: "json",
+  fontFamily: "Courier New",
+  fontSize: 16,
+};
+
+const settings = reactive({
+  language: "json",
+  fontFamily: "Courier New",
+  fontSize: 16,
+});
+
+const hour = ref(1);
 const link = ref("");
 const loading = ref(false);
 
-const handleOk = () => {
-  const json = editor.getValue();
+// 分享
+const shareHandle = () => {
+  const text = editor.getValue();
   loading.value = true;
   axios({
     method: "post",
     url: "/Api/AddJson",
     data: {
-      json: json,
+      text,
       hour: hour.value,
+      settings: currSettings,
     },
   })
     .then((r) => {
@@ -69,67 +138,135 @@ const handleOk = () => {
     })
     .catch((e) => {
       loading.value = false;
+      Message.error("请求失败");
     });
 };
 
+// 设置
+const settingsHandle = (isCache) => {
+  Object.assign(currSettings, settings);
+  editor.updateOptions({ ...currSettings });
+  const model = editor.getModel();
+  monaco.editor.setModelLanguage(model, currSettings.language);
+  settings_visible.value = false;
+  if (isCache) {
+    localStorage.setItem("settings", JSON.stringify(currSettings));
+  }
+};
+
+// 取消设置
+const settingsCancel = () => {
+  Object.assign(settings, currSettings);
+  settings_visible.value = false;
+};
+
+// 复制
 const copy = () => {
   if (link.value === "") {
-    Message.error("Please Mr. Into the link");
+    Message.error("先生成链接");
   } else {
     toClipboard(link.value)
       .then((_) => {
-        Message.success("Successful");
+        Message.success("复制成功");
       })
       .catch((_) => {
-        Message.error("Failure");
+        Message.error("复制失败");
       });
   }
 };
 
 onMounted(() => {
   document.body.setAttribute("arco-theme", "dark");
-
   if (editorContainer.value) {
-    editor = monaco.editor.create(editorContainer.value, {
-      value: "",
-      language: "json",
-      theme: "vs-dark",
-    });
+    loader.init().then((monacoInstance) => {
+      editor = monacoInstance.editor.create(editorContainer.value, {
+        value: "",
+        language: settings.language,
+        theme: "vs-dark",
+        fontFamily: settings.fontFamily,
+        fontSize: settings.fontSize,
+        automaticLayout: true,
+      });
 
-    window.addEventListener("resize", () => {
-      editor.layout();
-    });
-
-    editor.addAction({
-      id: "share-json",
-      label: "Share",
-      contextMenuGroupId: "navigation",
-      contextMenuOrder: 1,
-      run: function (editor) {
-        model_visible.value = true;
-      },
-    });
-    const params = new URLSearchParams(window.location.search);
-    const key = params.get("key");
-    if (key) {
-      axios({
-        method: "get",
-        url: "/Api/GetJson",
-        params: {
-          key,
+      editor.addAction({
+        id: "change-settings",
+        label: "设置",
+        contextMenuGroupId: "navigation",
+        contextMenuOrder: 0,
+        run: function (editor) {
+          settings_visible.value = true;
         },
-      })
-        .then((r) => {
-          if (r.data.json === "") {
-            Message.error("Does not exist or has expired");
-          } else {
-            editor.setValue(r.data.json);
-          }
+      });
+
+      editor.addAction({
+        id: "share-json",
+        label: "分享",
+        contextMenuGroupId: "navigation",
+        contextMenuOrder: 0.1,
+        run: function (editor) {
+          share_model_visible.value = true;
+        },
+      });
+
+      editor.addAction({
+        id: "fold-all",
+        label: "折叠所有",
+        contextMenuGroupId: "navigation",
+        contextMenuOrder: 0.2,
+        run: function (editor) {
+          editor.getAction("editor.foldAll").run();
+        },
+      });
+
+      editor.addAction({
+        id: "unfold-all",
+        label: "展开所有",
+        contextMenuGroupId: "navigation",
+        contextMenuOrder: 0.3,
+        run: function (editor) {
+          editor.getAction("editor.unfoldAll").run();
+        },
+      });
+
+      // 读取配置缓存
+      const settingsCache = localStorage.getItem("settings");
+      if (settingsCache) {
+        const settingsCacheObj = JSON.parse(settingsCache);
+        Object.assign(settings, settingsCacheObj);
+        settingsHandle(false);
+      }
+
+      // 读取key
+      const params = new URLSearchParams(window.location.search);
+      const key = params.get("key");
+      if (key) {
+        axios({
+          method: "get",
+          url: "/Api/GetJson",
+          params: {
+            key,
+          },
         })
-        .catch((_) => {
-          Message.error("Request failed");
-        });
-    }
+          .then((r) => {
+            if (r.data == null) {
+              Message.error("读取的内容为空");
+            } else {
+              Object.assign(settings, r.data.settings);
+              settingsHandle(false);
+              editor.setValue(r.data.text);
+            }
+          })
+          .catch((_) => {
+            Message.error("请求失败");
+          });
+      }
+    });
+  }
+});
+
+onUnmounted(() => {
+  if (editor.value) {
+    editor.value.dispose();
   }
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-
 import ArcoVue from "@arco-design/web-vue";
 import "@arco-design/web-vue/dist/arco.css";
-
-self.MonacoEnvironment = {
-  getWorker(_1: string, _2: string) {
-    return new jsonWorker();
-  },
-};
 
 createApp(App).use(ArcoVue).mount("#app");
