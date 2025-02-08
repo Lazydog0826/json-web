408a82bbd24baee9edd966eba7ed343381197472
hzc <huangzicheng@wexsu.com>
2025-02-08 16:24:37 +0800

    feat: 更新ui布局，删除字体与字体大小设置

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
-  <div class="editor-container" ref="editorContainer"></div>
+  <a-layout class="layout">
+    <a-layout-header class="header">
+      <a-space>
+        <a-select
+          style="width: 200px"
+          v-model="settings.language"
+          allow-search
+          @change="languageChange"
+        >
+          <a-option value="plaintext">PlainText</a-option>
+          <a-option value="javascript">JavaScript</a-option>
+          <a-option value="typescript">TypeScript</a-option>
+          <a-option value="css">CSS</a-option>
+          <a-option value="html">HTML</a-option>
+          <a-option value="json">JSON</a-option>
+          <a-option value="xml">XML</a-option>
+          <a-option value="yaml">YAML</a-option>
+        </a-select>
+        <a-button type="primary" @click="share_model_visible = true">
+          <template #icon>
+            <icon-link />
+          </template>
+          <template #default>分享</template>
+        </a-button>
+        <a-button type="primary" @click="download">
+          <template #icon>
+            <icon-download />
+          </template>
+          <template #default>下载</template>
+        </a-button>
+      </a-space>
+    </a-layout-header>
+
+    <a-layout-content>
+      <div class="editor-container" ref="editorContainer"></div>
+    </a-layout-content>
+  </a-layout>
 
   <a-modal v-model:visible="share_model_visible" :mask-closable="false">
     <template #title>分享</template>
     <div>
       <a-spin :loading="loading" style="width: 100%">
         <a-space direction="vertical" style="width: 100%">
-          <a-input-number
-            v-model="hour"
-            placeholder="过期时间 - 小时"
-            :min="1"
-            :max="10000"
-          />
+          <a-alert>过期时间 : 小时</a-alert>
+          <a-input-number v-model="hour" :min="1" :max="10000" />
           <a-link v-show="link !== ''" :href="link">{{ link }}</a-link>
           <a-link v-show="link !== ''" @click="copy">Copy</a-link>
         </a-space>
@@ -27,55 +59,6 @@
         </a-button>
         <a-button :loading="loading" type="primary" @click="shareHandle">
           生成链接
-        </a-button>
-      </a-space>
-    </template>
-  </a-modal>
-
-  <a-modal v-model:visible="settings_visible" :mask-closable="false">
-    <template #title>设置</template>
-    <div>
-      <a-form :model="settings" layout="vertical">
-        <a-form-item field="language" label="语言">
-          <a-select v-model="settings.language" allow-search>
-            <a-option value="plaintext">PlainText</a-option>
-            <a-option value="javascript">JavaScript</a-option>
-            <a-option value="typescript">TypeScript</a-option>
-            <a-option value="css">CSS</a-option>
-            <a-option value="html">HTML</a-option>
-            <a-option value="json">JSON</a-option>
-            <a-option value="xml">XML</a-option>
-            <a-option value="yaml">YAML</a-option>
-            <a-option value="markdown">Markdown</a-option>
-            <a-option value="csharp">C#</a-option>
-            <a-option value="rust">Rust</a-option>
-            <a-option value="sql">SQL</a-option>
-            <a-option value="bash">Bash</a-option>
-            <a-option value="powershell">PowerShell</a-option>
-            <a-option value="dockerfile">Dockerfile</a-option>
-            <a-option value="less">Less</a-option>
-            <a-option value="scss">SCSS</a-option>
-            <a-option value="sass">Sass</a-option>
-            <a-option value="stylus">Stylus</a-option>
-          </a-select>
-        </a-form-item>
-        <a-form-item field="fontFamily" label="字体">
-          <a-input v-model="settings.fontFamily" />
-        </a-form-item>
-        <a-form-item field="fontSize" label="字体大小">
-          <a-input-number v-model="settings.fontSize" :min="14" :max="30" />
-        </a-form-item>
-      </a-form>
-    </div>
-    <template #footer>
-      <a-space>
-        <a-button @click="settingsCancel"> 取消 </a-button>
-        <a-button
-          :loading="loading"
-          type="primary"
-          @click="settingsHandle(true)"
-        >
-          设置
         </a-button>
       </a-space>
     </template>
@@ -99,13 +82,6 @@ const editorContainer = ref(null);
 let editor = null;
 
 const share_model_visible = ref(false);
-const settings_visible = ref(false);
-
-let currSettings = {
-  language: "json",
-  fontFamily: "Courier New",
-  fontSize: 16,
-};
 
 const settings = reactive({
   language: "json",
@@ -127,7 +103,7 @@ const shareHandle = () => {
     data: {
       text,
       hour: hour.value,
-      settings: currSettings,
+      settings,
     },
   })
     .then((r) => {
@@ -143,20 +119,16 @@ const shareHandle = () => {
 
 // 设置
 const settingsHandle = (isCache) => {
-  Object.assign(currSettings, settings);
-  editor.updateOptions({ ...currSettings });
   const model = editor.getModel();
-  monaco.editor.setModelLanguage(model, currSettings.language);
-  settings_visible.value = false;
+  monaco.editor.setModelLanguage(model, settings.language);
   if (isCache) {
-    localStorage.setItem("settings", JSON.stringify(currSettings));
+    localStorage.setItem("settings", JSON.stringify(settings));
   }
 };
 
-// 取消设置
-const settingsCancel = () => {
-  Object.assign(settings, currSettings);
-  settings_visible.value = false;
+// 语言切换事件
+const languageChange = (language) => {
+  settingsHandle(true);
 };
 
 // 复制
@@ -174,6 +146,31 @@ const copy = () => {
   }
 };
 
+// 下载
+const download = () => {
+  const blob = new Blob([editor.getValue()], { type: "text/plain" });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "download.";
+
+  switch (settings.language) {
+    case "plaintext":
+      link.download += "txt";
+      break;
+    case "javascript":
+      link.download += "js";
+      break;
+    case "typescript":
+      link.download += "ts";
+      break;
+    default:
+      link.download += settings.language;
+      break;
+  }
+  link.click();
+  link.remove();
+};
+
 onMounted(() => {
   document.body.setAttribute("arco-theme", "dark");
   if (editorContainer.value) {
@@ -187,25 +184,25 @@ onMounted(() => {
         automaticLayout: true,
       });
 
-      editor.addAction({
-        id: "change-settings",
-        label: "设置",
-        contextMenuGroupId: "navigation",
-        contextMenuOrder: 0,
-        run: function (editor) {
-          settings_visible.value = true;
-        },
-      });
+      // editor.addAction({
+      //   id: "change-settings",
+      //   label: "设置",
+      //   contextMenuGroupId: "navigation",
+      //   contextMenuOrder: 0,
+      //   run: function (editor) {
+      //     settings_visible.value = true;
+      //   },
+      // });
 
-      editor.addAction({
-        id: "share-json",
-        label: "分享",
-        contextMenuGroupId: "navigation",
-        contextMenuOrder: 0.1,
-        run: function (editor) {
-          share_model_visible.value = true;
-        },
-      });
+      // editor.addAction({
+      //   id: "share-json",
+      //   label: "分享",
+      //   contextMenuGroupId: "navigation",
+      //   contextMenuOrder: 0.1,
+      //   run: function (editor) {
+      //     share_model_visible.value = true;
+      //   },
+      // });
 
       editor.addAction({
         id: "fold-all",
@@ -271,6 +268,18 @@ onUnmounted(() => {
 </script>
 
 <style scoped>
+.layout {
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  .header {
+    height: 48px;
+    line-height: 48px;
+    padding-left: 12px;
+    border-bottom: 1px solid var(--color-fill-2);
+  }
+}
+
 .editor-container {
   width: 100%;
   height: 100%;
